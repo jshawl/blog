eca1f85e8e7b9849e9056238190c42c388303a0d
Conner McDaniel <connermcd@gmail.com>
2013-08-01 22:56:13 -0500

    Updated youtube plugin.

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -1,1 +1,15 @@
-../extras/octopress-responsive-video-embed/youtube.rb
+module Jekyll
+  class Youtube < Liquid::Tag
+
+    def initialize(name, id, tokens)
+      super
+      @id = id
+    end
+
+    def render(context)
+      %(<div class="embed-video-container"><iframe width=640 height=510 src="http://www.youtube.com/embed/#{@id}"></iframe></div>)
+    end
+  end
+end
+
+Liquid::Template.register_tag('youtube', Jekyll::Youtube)
